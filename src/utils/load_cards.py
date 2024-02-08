aef2f78bfdec17ba60991cff6b23cbaa2ebee23f
Hugues Marandeau <h.marandeau@gmail.com>
2024-02-08 11:59:38 +0100

    Update logic to load new cards data into json files

--- src/utils/load_cards.py
+++ src/utils/load_cards.py
@@ -4,6 +4,12 @@ import os
 import requests
 
 
+ASSETS_CARDS_PATH = "assets/"
+CARDS_ALL_FILE_NAME = "cards.json"
+CARDS_3_STARS_FILE_NAME = "cards_3.json"
+CARDS_4_STARS_FILE_NAME = "cards_4.json"
+CARDS_5_STARS_FILE_NAME = "cards_5.json"
+
 ASSETS_IMAGES_PATH = "assets/images/"
 ASSETS_IMAGES__BLUE_PATH = f"{ASSETS_IMAGES_PATH}blue/"
 ASSETS_IMAGES__RED_PATH = f"{ASSETS_IMAGES_PATH}red/"
@@ -11,7 +17,8 @@ ASSETS_IMAGES__RED_PATH = f"{ASSETS_IMAGES_PATH}red/"
 RAELYS_API_GET_ALL_CARDS = "https://triad.raelys.com/api/cards"
 RAELYS_API_RESULT = "results"
 
-WRITE_BINARY = "wb"
+FILE_WRITE_BINARY = "wb"
+FILE_WRITE = "w"
 
 
 def parse_card_info(card):
@@ -38,17 +45,35 @@ def download_and_save_new_cards():
         raise requests.HTTPError(response.content)
 
     response_content = json.loads(response.content)
+    all_cards = []
+    cards_3_stars = []
+    cards_4_stars = []
+    cards_5_stars = []
 
     for card in response_content[RAELYS_API_RESULT]:
-        # print(card)
-        print(parse_card_info(card))
-        maybe_get_card_images(
+        print(f"Iterating over card {card['id']}")
+        maybe_download_and_save_card_images(
             card["id"], card["image"], card["image_blue"], card["image_red"]
         )
-        break
+        formatted_card = parse_card_info(card)
+        all_cards.append(formatted_card)
+        match formatted_card["stars"]:
+            case 3:
+                cards_3_stars.append(formatted_card)
+            case 4:
+                cards_4_stars.append(formatted_card)
+            case 5:
+                cards_5_stars.append(formatted_card)
+            case _:
+                # We don't really care about cards that have less than 3 stars.
+                continue
+
+    save_cards(all_cards, cards_3_stars, cards_4_stars, cards_5_stars)
 
 
-def maybe_get_card_images(card_id, image_url, image_blue_url, image_red_url):
+def maybe_download_and_save_card_images(
+    card_id, image_url, image_blue_url, image_red_url
+):
     card_image_path = f"{ASSETS_IMAGES_PATH}{card_id}.png"
     card_image_blue_path = f"{ASSETS_IMAGES__BLUE_PATH}{card_id}.png"
     card_image_red_path = f"{ASSETS_IMAGES__RED_PATH}{card_id}.png"
@@ -68,5 +93,17 @@ def download_and_save_card_image(image_url, destination_file_path):
     if response.status_code != 200:
         raise requests.HTTPError(response.content)
 
-    with open(destination_file_path, WRITE_BINARY) as destination_image:
+    with open(destination_file_path, FILE_WRITE_BINARY) as destination_image:
         destination_image.write(response.content)
+
+
+def save_cards(all_cards, cards_3_stars, cards_4_stars, cards_5_stars):
+    write_cards_file(all_cards, f"{ASSETS_CARDS_PATH}{CARDS_ALL_FILE_NAME}")
+    write_cards_file(cards_3_stars, f"{ASSETS_CARDS_PATH}{CARDS_3_STARS_FILE_NAME}")
+    write_cards_file(cards_4_stars, f"{ASSETS_CARDS_PATH}{CARDS_4_STARS_FILE_NAME}")
+    write_cards_file(cards_5_stars, f"{ASSETS_CARDS_PATH}{CARDS_5_STARS_FILE_NAME}")
+
+
+def write_cards_file(cards_data, destination_file_path):
+    with open(destination_file_path, FILE_WRITE) as destination_file:
+        json.dump({"cards": cards_data}, destination_file)
