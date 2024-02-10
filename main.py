6923b444f52a143d61b9e56ad7a39d9892731edb
Hugues Marandeau <h.marandeau@gmail.com>
2024-02-10 10:10:37 +0100

    working image recognition for cards with keras and vgg16 features comparison

--- main.py
+++ main.py
@@ -1,17 +1,55 @@
 # import cProfile
+import cv2
+import numpy as np
+
 from src.models.board import DRAW
+from src.ocr.board_recognition import test_board_recognition
+from src.ocr.card_recognition import test_one_to_many
 from src.simulate_game import play_algorithm_negamax
 from src.simulate_game import play_algorithm_random
 from src.simulate_game import simulate_game_from_start
 from src.utils.download_cards import download_and_save_new_cards
 
+
+def get_one_image(images):
+    img_list = []
+    padding = 200
+    for img in images:
+        img_list.append(cv2.imread(img))
+    max_width = []
+    max_height = 0
+    for img in img_list:
+        max_width.append(img.shape[0])
+        max_height += img.shape[1]
+    w = np.max(max_width)
+    h = max_height + padding
+
+    # create a new array with a size large enough to contain all the images
+    final_image = np.zeros((h, w, 3), dtype=np.uint8)
+
+    current_y = (
+        0  # keep track of where your current image was last placed in the y coordinate
+    )
+    for image in img_list:
+        # add an image to the final array and increment the y coordinate
+        final_image[
+            current_y : image.shape[0] + current_y, : image.shape[1], :  # noqa
+        ] = image
+        current_y += image.shape[0]
+
+    return final_image
+
+
 if __name__ == "__main__":
     # TODO() Command line params
-    if False:
+    should_download_new_cards = False
+    should_simulate_game = False
+
+    if should_download_new_cards:
         download_and_save_new_cards()
 
     statistics = {"DRAW": 0, "RED": 0, "BLUE": 0}
-    while True:
+    while should_simulate_game:
         board = simulate_game_from_start(
             first_player_algorithm=play_algorithm_random,
             second_player_algorithm=play_algorithm_negamax,
@@ -27,3 +65,13 @@ if __name__ == "__main__":
             statistics[DRAW] += 1
 
         print(statistics)
+
+    test_board_recognition()
+
+    for test_card_id in range(6, 10):
+        result_id = test_one_to_many(test_card_id)
+        final_image__ = get_one_image(
+            [f"assets/test_images/{test_card_id}.png", f"assets/images/{result_id}.png"]
+        )
+        cv2.imshow("", final_image__)
+        cv2.waitKey(0)
---
+++ src/ocr/card_recognition.py
@@ -0,0 +1,56 @@
+import os.path
+
+import numpy as np
+from keras.applications.vgg16 import preprocess_input
+from keras.applications.vgg16 import VGG16
+from keras.models import Model
+from keras.preprocessing import image
+from PIL import Image
+
+# https://stackoverflow.com/a/71967670
+
+
+def extract(img, model):
+    img = img.resize((224, 224))  # Resize the image
+    img = img.convert("RGB")  # Convert the image color space
+    x = image.img_to_array(img)  # Reformat the image
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+    # TODO send all x at once
+    # https://www.tensorflow.org/api_docs/python/tf/keras/Model#predict
+    feature = model.predict(x)[0]  # Extract Features
+    return feature / np.linalg.norm(feature)
+
+
+def test_one_to_many(input_img_name):
+    base_model = VGG16(weights="imagenet")
+    model = Model(inputs=base_model.input, outputs=base_model.get_layer("fc1").output)
+
+    # Iterate through images and extract Features
+    images = [f"assets/images/{i}.png" for i in range(1, 404)]
+
+    if os.path.exists("tmp_features.npy"):
+        all_features = np.load("tmp_features.npy")
+    else:
+        all_features = np.zeros(shape=(len(images), 4096))
+
+        for i in range(len(images)):
+            feature = extract(img=Image.open(images[i]), model=model)
+            all_features[i] = np.array(feature)
+
+        import ipdb
+
+        ipdb.set_trace()
+        np.save("tmp_features", np.array(all_features))
+
+    # Match image
+    query = extract(
+        img=Image.open(f"assets/test_images/{input_img_name}.png"), model=model
+    )  # Extract its features
+    dists = np.linalg.norm(
+        all_features - query, axis=1
+    )  # Calculate the similarity (distance) between images
+    ids = np.argsort(dists)[:5]  # Extract 5 images that have lowest distance
+    # print(ids)
+    print(f"Card should be: {ids[0]+1}")
+    return ids[0] + 1
